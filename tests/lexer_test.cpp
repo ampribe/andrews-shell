2b452991f789ccdd5a1450c4b168a05806777d7a
Andrew Pribe <ampribe@uchicago.edu>
2025-01-08 15:40:20 -0600

    modified tests for new lexer return type

--- tests/lexer_test.cpp
+++ tests/lexer_test.cpp
@@ -1,145 +1,145 @@
 #include <gtest/gtest.h>
 #include <string>
-#include <utility>
+#include <variant>
 #include "lexer.h"
-#include <token.h>
+#include "token.h"
+#include "shellerror.h"
 
 class LexerTest : public testing::Test {
 protected:
 	void SetUp() override {}
 	LexerTest() {}
-	void testLexer(std::string input, std::vector<std::pair<Type, std::string>> expected) {
+	void testLexer(std::string input, std::vector<std::variant<Token, ShellError>> expected) {
 		Lexer lexer(input);
-		Token tok;
-		for (const auto& [type, value] : expected) {
+		std::variant<Token, ShellError> tok;
+		for (const auto& value: expected) {
 			tok = lexer.getToken();
-			EXPECT_EQ(tok.type, type);
-			EXPECT_EQ(tok.value, value);
+			EXPECT_EQ(tok, value);
 		}
 	}
 };
 
 TEST_F(LexerTest, EmptyInput) {
 	std::string input = "";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::END, ""},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::END, ""},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, SingleCommand1) {
 	std::string input = "echo asdf";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::LITERAL, "echo"},
-		{Type::LITERAL, "asdf"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::LITERAL, "echo"},
+		Token {Type::LITERAL, "asdf"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, SingleCommand2) {
 	std::string input = "echo asdf 123 \"%%%\"";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::LITERAL, "echo"},
-		{Type::LITERAL, "asdf"},
-		{Type::LITERAL, "123"},
-		{Type::QUOTE, "%%%"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::LITERAL, "echo"},
+		Token {Type::LITERAL, "asdf"},
+		Token {Type::LITERAL, "123"},
+		Token {Type::QUOTE, "%%%"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, SingleCommand3) {
 	std::string input = "		asdf  123 \"%%%\"	";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::LITERAL, "asdf"},
-		{Type::LITERAL, "123"},
-		{Type::QUOTE, "%%%"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::LITERAL, "asdf"},
+		Token {Type::LITERAL, "123"},
+		Token {Type::QUOTE, "%%%"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, Pipeline1) {
 	std::string input = "		asdf  123 | \"%%%\"	";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::LITERAL, "asdf"},
-		{Type::LITERAL, "123"},
-		{Type::PIPE, "|"},
-		{Type::QUOTE, "%%%"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::LITERAL, "asdf"},
+		Token {Type::LITERAL, "123"},
+		Token {Type::PIPE, "|"},
+		Token {Type::QUOTE, "%%%"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, Pipeline2) {
 	std::string input = "| |		|		asdf  123 | \"%%%\"	";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::PIPE, "|"},
-		{Type::PIPE, "|"},
-		{Type::PIPE, "|"},
-		{Type::LITERAL, "asdf"},
-		{Type::LITERAL, "123"},
-		{Type::PIPE, "|"},
-		{Type::QUOTE, "%%%"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::PIPE, "|"},
+		Token {Type::PIPE, "|"},
+		Token {Type::PIPE, "|"},
+		Token {Type::LITERAL, "asdf"},
+		Token {Type::LITERAL, "123"},
+		Token {Type::PIPE, "|"},
+		Token {Type::QUOTE, "%%%"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, MultipleCommands) {
 	std::string input = "| |;		|		asdf  123 | \"%%%\"	";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::PIPE, "|"},
-		{Type::PIPE, "|"},
-		{Type::SEMI, ";"},
-		{Type::PIPE, "|"},
-		{Type::LITERAL, "asdf"},
-		{Type::LITERAL, "123"},
-		{Type::PIPE, "|"},
-		{Type::QUOTE, "%%%"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::PIPE, "|"},
+		Token {Type::PIPE, "|"},
+		Token {Type::SEMI, ";"},
+		Token {Type::PIPE, "|"},
+		Token {Type::LITERAL, "asdf"},
+		Token {Type::LITERAL, "123"},
+		Token {Type::PIPE, "|"},
+		Token {Type::QUOTE, "%%%"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, Redirection1) {
 	std::string input = ">> \\<\\<";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::REDIRECT, ">>"},
-		{Type::LITERAL, "<<"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::REDIRECT, ">>"},
+		Token {Type::LITERAL, "<<"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, Redirection2) {
 	std::string input = "2>&1 &> 2>> 1>&2 >";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::REDIRECT, "2>&1"},
-		{Type::REDIRECT, "&>"},
-		{Type::REDIRECT, "2>>"},
-		{Type::REDIRECT, "1>&2"},
-		{Type::REDIRECT, ">"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::REDIRECT, "2>&1"},
+		Token {Type::REDIRECT, "&>"},
+		Token {Type::REDIRECT, "2>>"},
+		Token {Type::REDIRECT, "1>&2"},
+		Token {Type::REDIRECT, ">"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
 
 TEST_F(LexerTest, Redirection3) {
 	std::string input = "2>&1 <<<< &> 2>> 1>&2 >";
-	std::vector<std::pair<Type, std::string>> expected = {
-		{Type::REDIRECT, "2>&1"},
-		{Type::REDIRECT, "<"},
-		{Type::REDIRECT, "<"},
-		{Type::REDIRECT, "<"},
-		{Type::REDIRECT, "<"},
-		{Type::REDIRECT, "&>"},
-		{Type::REDIRECT, "2>>"},
-		{Type::REDIRECT, "1>&2"},
-		{Type::REDIRECT, ">"},
-		{Type::END, ""}
+	std::vector<std::variant<Token, ShellError>> expected = {
+		Token {Type::REDIRECT, "2>&1"},
+		Token {Type::REDIRECT, "<"},
+		Token {Type::REDIRECT, "<"},
+		Token {Type::REDIRECT, "<"},
+		Token {Type::REDIRECT, "<"},
+		Token {Type::REDIRECT, "&>"},
+		Token {Type::REDIRECT, "2>>"},
+		Token {Type::REDIRECT, "1>&2"},
+		Token {Type::REDIRECT, ">"},
+		Token {Type::END, ""}
 	};
 	testLexer(input, expected);
 }
