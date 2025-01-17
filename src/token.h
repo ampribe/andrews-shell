5b0594e8a9bcbcca856117461586475d3a7ca6ec
Andrew Pribe <ampribe@uchicago.edu>
2025-01-16 21:43:47 -0600

    implemented tostring for Token

--- src/token.h
+++ src/token.h
@@ -6,11 +6,16 @@
 enum Type { PIPE, SEMI, QUOTE, LITERAL, END, REDIRECT, CONTROL };
 
 struct Token {
-	Type type;
-	std::string value;
+	Type type{Type::END};
+	std::string value{"END"};
 
 	bool operator==(const Token& other) const {
 		return type == other.type && value == other.value;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Token& token) {
+		os << token.type << " " << token.value;
+		return os;
 	}
 };
 
